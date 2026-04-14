3c901965ed89aac938545332b0bdbae20dff0289
sirkoffs <elumariofreitasbarrinhi@gmail.com>
2026-04-14 19:22:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -60,9 +60,9 @@ async def rotacao_status():
 
 
             atividades = [
-                discord.Activity(type=discord.ActivityType.playing, name="Comendo o lhzin"),
-                discord.Activity(type=discord.ActivityType.playing, name="Comendo o lhzin"),
-                discord.Activity(type=discord.ActivityType.playing, name="Comendo o lhzin")
+                discord.Activity(type=discord.ActivityType.playing, name="After Effects 1864"),
+                discord.Activity(type=discord.ActivityType.playing, name="After Effects 1864"),
+                discord.Activity(type=discord.ActivityType.playing, name="After Effecte 1864")
             ]
 
 
